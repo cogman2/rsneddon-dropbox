57f59c8616bd9260d7882830308728672fcf95c3
Robert Sneddon <sneddon_robert@bah.com>
2016-07-17 16:44:35 -0400

    A new exercise

--- python/ex45.py
+++ python/ex45.py
@@ -1,16 +1,20 @@
 # This is the "make the game" exercise.
 # Do it well!
 #
-import rooms  # the data
-import runner # the interface
+import Rooms  # the data
+import Runner # the interface
 
 class Engine(object): # the controller
    
     def __init__(self, rooms_map):
         self.rooms_map = rooms_map
+        self.runner = Runner()
 
     def play(self):
         current_room = self.rooms_map.opening_scene()
         last_room = self.rooms_map.next_room('finished')
     
     current_room.enter()
+
+rooms_map = Rooms()
+game = Engine(rooms_map)
