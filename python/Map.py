f162a2b3fe90997de411d43b133c7f249860b72a
Robert Sneddon <robertsneddon@cnn-gpu-04.ec2.internal>
2016-07-21 13:09:46 +0000

    Updated stuff.

---
+++ python/Map.py
@@ -0,0 +1,26 @@
+#
+# The interface for the game
+#
+from random import randint
+class Map(object):
+    scenes = {
+        'lobby': Lobby(),
+        'hallway': Hallway(),
+        'sinking': Sinking(),
+        'spiked_ceiling': Spiked_ceiling(),
+        'dungeon': Dungeon(),
+        'moving_walls': Moving_walls(),
+        'escape_tunnel': Escape_tunnel()
+    }
+
+    def __init__(self, start_scene):
+        self.start_scene = start_scene
+
+    def next_scene(self, scene_name):
+        val = Map.scenes.get(scene_name)
+        return val
+
+    def opening_scene(self):
+        return self.next_scene(self.start_scene)
+
+
